76e95818160ebbe94ec0ee865709168aa6f450aa
RohnRoy <ronroy579@gmail.com>
2025-02-07 13:03:32 +0530

    Portfolio initiated

---
+++ src/data/headerData.js
@@ -0,0 +1,11 @@
+import resume from "../assets/pdf/Rohn_Roy_Resume.pdf";
+import passport from "../assets/png/Passport.jpg";
+
+export const headerData = {
+  name: "Rohn Roy",
+  title: "Software Engineer",
+  desciption:
+    "The server logs were silent, but that wasn't what caught his eye. It was the metrics.The deep red spike, a color that shouldn't be there, but was. He knew what was coming and hoped his code was ready.",
+  image: "src/assets/png/Passport.jpg",
+  resumePdf: resume,
+};
