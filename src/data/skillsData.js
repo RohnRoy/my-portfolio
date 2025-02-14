412bbb6695e531b27d43370a7bf073cf7f319be4
RohnRoy <ronroy579@gmail.com>
2025-02-14 14:24:40 +0530

    Update dependencies and add NodeJS skill icon

--- src/data/skillsData.js
+++ src/data/skillsData.js
@@ -10,12 +10,12 @@
     'HTML',
     'MongoDB',
     'JavaScript',
-    'Node JS',
+    'NodeJS',
     'Figma',
 ]
 
 // Choose your skills from below. Make sure it's in the same format and spelled correctly.
-// Couldn't find the required skills? Raise an issue on github at https://github.com/hhhrrrttt222111/developer-portfolio/issues/new
+
 
 
 // AVAILABLE SKILLS
--- src/utils/skillsImage.js
+++ src/utils/skillsImage.js
@@ -58,6 +58,7 @@ import strapi from '../assets/svg/skills/strapi.svg'
 import tensorflow from '../assets/svg/skills/tensorflow.svg'
 import webix from '../assets/svg/skills/webix.svg'
 import wordpress from '../assets/svg/skills/wordpress.svg'
+import nodejs from '../assets/svg/skills/nodejs.svg'
 
 import azure from '../assets/svg/skills/azure.svg'
 import blender from '../assets/svg/skills/blender.svg'
@@ -227,6 +228,9 @@ export const skillsImage = (skill) => {
             return wolframalpha;
         case 'canva':
             return canva;
+        case 'nodejs':
+            return nodejs;
+            
         default:
             break;
     }
